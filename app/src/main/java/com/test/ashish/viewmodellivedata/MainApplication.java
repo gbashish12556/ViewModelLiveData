2b8df9bb849052e104a1c193e777bdf786498a47
Ashish Gupta <ashish.gupta@truebil.com>
2019-01-23 00:31:12 +0530

    removed leakcanary from app

--- app/src/main/java/com/test/ashish/viewmodellivedata/MainApplication.java
+++ app/src/main/java/com/test/ashish/viewmodellivedata/MainApplication.java
@@ -14,11 +14,11 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
+//        if (LeakCanary.isInAnalyzerProcess(this)) {
+//            // This process is dedicated to LeakCanary for heap analysis.
+//            // You should not init your app in this process.
+//            return;
+//        }
+//        LeakCanary.install(this);
     }
 }
